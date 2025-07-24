2b3e2922317d2d9067f3531e9172705a6dc580f3
yeongbinbae <yeongbinbae01@gmail.com>
2025-07-24 17:11:36 +0900

    first commit

---
+++ src/main/java/inq/upfit/UpfitApplication.java
@@ -0,0 +1,13 @@
+package inq.upfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpfitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpfitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inq/upfit/UpfitApplicationTests.java
@@ -0,0 +1,13 @@
+package inq.upfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpfitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
