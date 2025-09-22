14b8ea2e53c65e136e1f2f765c95487ae328556a
JONGTAE02 <qwx0677@naver.com>
2025-09-22 19:26:18 +0900

    feat:CommentRepository 추가 (#22)

---
+++ src/main/java/inq/upfit/repository/CommentRepository.java
@@ -0,0 +1,8 @@
+package inq.upfit.repository;
+
+import inq.upfit.domain.master.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
