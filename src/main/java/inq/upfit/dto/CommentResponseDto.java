a4c91c178f75fc44112c92e6977b1aa37222cf52
JONGTAE02 <qwx0677@naver.com>
2025-09-22 19:20:07 +0900

    feat:CommentResponseDto 추가 (#22)

---
+++ src/main/java/inq/upfit/dto/CommentResponseDto.java
@@ -0,0 +1,30 @@
+package inq.upfit.dto;
+
+import inq.upfit.domain.master.Comment;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class CommentResponseDto {
+    private Long id;
+    private String content;
+    private String attachment;
+    private String writerName;
+    private LocalDateTime regDate;
+    private LocalDateTime modDate;
+
+    public static CommentResponseDto from(Comment comment) {
+        return  CommentResponseDto.builder()
+                .id(comment.getId())
+                .content(comment.getContent())
+                .attachment(comment.getPost().getAttachment())
+                .writerName(comment.getWriter().getName())
+                .regDate(comment.getRegDate())
+                .modDate(comment.getModDate())
+                .build();
+
+    }
+}
