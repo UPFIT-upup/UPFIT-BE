c19f9ef1e44ac6114de6ac80c0a2f7929138a1b0
JONGTAE02 <qwx0677@naver.com>
2025-09-26 22:40:31 +0900

    feat: CommentRepository에 조회 쿼리 메서드 추가(#22)

--- src/main/java/inq/upfit/repository/CommentRepository.java
+++ src/main/java/inq/upfit/repository/CommentRepository.java
@@ -2,7 +2,17 @@ package inq.upfit.repository;
 
 import inq.upfit.domain.master.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    // 특정 게시글의 모든 댓글 조회 (작성일 순)
+    List<Comment> findByPostIdOrderByRegDateAsc(Long postId);
+
+    // 특정 게시글의 댓글 개수
+    long countByPostId(Long postId);
+
+    // 특정 사용자가 작성한 댓글 조회
+    List<Comment> findByWriterIdOrderByRegDateDesc(Long writerId);
 }
