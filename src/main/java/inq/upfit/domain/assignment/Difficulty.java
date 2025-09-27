9208f4e5d53a2f0eaec37eab22de15bd9a16b87a
EverJun2 <deverjun2@gmail.com>
2025-09-28 00:14:40 +0900

    Feat: 과제 관련 Enum 작성 (#25)

---
+++ src/main/java/inq/upfit/domain/assignment/Difficulty.java
@@ -0,0 +1,8 @@
+package inq.upfit.domain.assignment;
+
+//문제 난이도
+public enum Difficulty {
+    EASY,
+    MEDIUM,
+    HIGH
+}
---
+++ src/main/java/inq/upfit/domain/assignment/SubmitType.java
@@ -0,0 +1,8 @@
+package inq.upfit.domain.assignment;
+
+//제출방식 타입
+public enum SubmitType {
+    IMAGE,
+    LINK,
+    ANSWER
+}
