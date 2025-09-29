8b175a075fb460d51d446bff030b717560164eaa
yeongbinbae <yeongbinbae01@gmail.com>
2025-09-29 20:19:30 +0900

    feat: 관리자 과제 생성 컨트롤러 구현 (#29)

--- src/main/java/inq/upfit/controller/AssignmentController.java
+++ src/main/java/inq/upfit/controller/AssignmentController.java
@@ -1,5 +1,27 @@
 package inq.upfit.controller;
 
-public class AssignmentController {
+import inq.upfit.domain.assignment.Assignment;
+import inq.upfit.dto.AssignmentCreateRequest;
+import inq.upfit.dto.AssignmentResponse;
+import inq.upfit.service.AssignmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
+@RestController
+public class AssignmentController {
+    private final AssignmentService assignmentService;
+
+    @PostMapping("/api/admin/assignments")
+    public ResponseEntity<AssignmentResponse> createAssignment(
+            Long adminId,
+            @RequestBody AssignmentCreateRequest createRequest
+    ) {
+        Assignment assignment = assignmentService.createAssignment(adminId, createRequest);
+
+        return ResponseEntity.ok(AssignmentResponse.fromAssignment(assignment));
+    }
 }
